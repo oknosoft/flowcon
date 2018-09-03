ff7dfaa496d70a308e1d4866eac242488b9577ef
unpete <info@oknosoft.ru>
2018-09-04 00:46:15 +0500

    (#185) css в Table

--- src/components/FakeDiagram/Table.js
+++ src/components/FakeDiagram/Table.js
@@ -13,6 +13,17 @@ import {chartData} from './Bar';
 import ReactDataGrid from 'metadata-external/react-data-grid.min';
 
 
+function Formatter({value}) {
+  if(value && typeof value === 'object') {
+    const val = value.presentation || value.value;
+    return <div title={val} style={value.css}>{val}</div>;
+  }
+  else {
+    return <div title={value}>{value}</div>;
+  }
+
+}
+
 function Table ({width, height, data, isFullscreen}) {
   if(isFullscreen) {
     width = window.innerWidth - 16;
@@ -22,9 +33,9 @@ function Table ({width, height, data, isFullscreen}) {
     height = width <= 600 ? width * 1.2 : width / 2.6;
   }
 
-  const _rows = chartData(data);
+  const {rows} = data;
   function rowGetter(i) {
-    return _rows[i];
+    return rows[i];
   }
 
   function transform(column) {
@@ -48,6 +59,7 @@ function Table ({width, height, data, isFullscreen}) {
     else if(typeof column.width === 'string') {
       res.width = width * parseFloat(column.width.replace('%', '')) / 100;
     }
+    res.formatter = Formatter;
     return res;
   }
 
@@ -55,7 +67,7 @@ function Table ({width, height, data, isFullscreen}) {
     <ReactDataGrid
       columns={data.columns.map(transform)}
       rowGetter={rowGetter}
-      rowsCount={_rows.length}
+      rowsCount={rows.length}
       minHeight={Math.min(height, 320)}
       minWidth={width}
       minColumnWidth={100}
