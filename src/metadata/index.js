7c39b3664a990179c70874beea50be9bab797bef
unpete <info@oknosoft.ru>
2018-10-08 12:02:43 +0500

    (#284) 2.0.17-beta.9

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -1,16 +1,16 @@
 
 // конструктор metadata.js
-import MetaEngine from 'metadata-core/index.min';
-import plugin_pouchdb from 'metadata-pouchdb/index.min';
-import plugin_ui from 'metadata-abstract-ui/index.min';
-import plugin_ui_tabulars from 'metadata-abstract-ui/tabulars.min';
-import plugin_superlogin from 'metadata-superlogin/index.min';
+// import MetaEngine from 'metadata-core/index.min';
+// import plugin_pouchdb from 'metadata-pouchdb/index.min';
+// import plugin_ui from 'metadata-abstract-ui/index.min';
+// import plugin_ui_tabulars from 'metadata-abstract-ui/tabulars.min';
+// import plugin_superlogin from 'metadata-superlogin/index.min';
 
-// import MetaEngine from 'metadata-core';
-// import plugin_pouchdb from 'metadata-pouchdb';
-// import plugin_ui from 'metadata-abstract-ui';
-// import plugin_ui_tabulars from 'metadata-abstract-ui/tabulars';
-// import plugin_superlogin from 'metadata-superlogin';
+import MetaEngine from 'metadata-core';
+import plugin_pouchdb from 'metadata-pouchdb';
+import plugin_ui from 'metadata-abstract-ui';
+import plugin_ui_tabulars from 'metadata-abstract-ui/tabulars';
+import plugin_superlogin from 'metadata-superlogin';
 
 import plugin_react from 'metadata-react/plugin';
 
